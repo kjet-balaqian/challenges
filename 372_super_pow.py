2423c53597f10c8a6ecf59ce4837ff85edcdf684
kjet-balaqian <jerome.yutai.shen@gmail.com>
2022-06-11 15:05:18 -0700

    N Queen

--- 372_super_pow.py
+++ 372_super_pow.py
@@ -5,3 +5,25 @@ Created on Jun 10 04:39:10 2022
 @author: Jerome Yutai Shen
 
 """
+from typing import List
+
+
+def mod(x):
+    return x % 1337
+
+
+class Solution:
+    """
+    @param a: the given number a
+    @param b: the given array
+    @return: the result
+    """
+    def superPow(self, a: int, b: List[int]) -> int:
+        # Write your code here
+        if a == 0:
+            return 0
+        ans = 1
+
+        for num in b:
+            ans = mod(mod(ans ** 10) * mod(a ** num))
+        return ans
--- 51_N_green.py
+++ 51_N_green.py
@@ -38,3 +38,41 @@ def solveNQueens(n):
 
     dfs(0)
     return res
+
+
+
+def solveNQueens( n):
+    li = [str(i) for i in range(n)]
+    result = []
+    dfs(result, "", li)
+    #return result
+    return constructGraph(result)
+
+    
+def dfs(result, path, nums):
+    if not nums:
+        result.append(path)
+        return
+    for i in range(len(nums)):
+        if not checkValid(path, nums[i]): continue
+        dfs(result, path+nums[i], nums[:i]+nums[i+1:])
+
+
+def checkValid(path, num):
+    if num in path: return False
+    for i in range(len(path)):
+        if abs(int(num) - int(path[i])) == abs(len(path)-i):
+            return False
+    return True
+
+
+def constructGraph( result):
+    matrix = []
+    for path in result:
+        row = []
+        for c in path:
+            queenStr = ['.' for i in range(len(path))]
+            queenStr[int(c)] = 'Q'
+            row.append(''.join(queenStr))
+        matrix.append(row[:])
+    return matrix
