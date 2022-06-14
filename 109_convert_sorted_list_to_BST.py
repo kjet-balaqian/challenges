0cdd932793a654500ef555aec534e7e2a6e383f5
kjet-balaqian <104302281+kjet-balaqian@users.noreply.github.com>
2022-06-14 13:20:56 -0700

    Add files via upload

---
+++ 109_convert_sorted_list_to_BST.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jun 14 05:11:51 2022
+
+@author: Jerome Yutai Shen
+
+"""
+from typing import Optional, List
+from common_classes import BinaryTreeNode, ListNode
+
+
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[BinaryTreeNode]:
+        return self.dfs(head)
+
+    def dfs(self, head):
+
+        if head == None:
+            return None
+
+        if head.next == None:
+            return BinaryTreeNode(head.val)
+
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = head
+        slow = dummy
+
+        while fast != None and fast.next != None:
+            fast = fast.next.next
+            slow = slow.next
+
+        temp = slow.next
+        slow.next = None
+        parent = BinaryTreeNode(temp.val)
+
+        parent.left = self.dfs(head)
+        parent.right = self.dfs(temp.next)
+
+        return parent
+
+
+if __name__ == "__main__":
+    pass
