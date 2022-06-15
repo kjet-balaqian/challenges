9e517c7c02d607c533e03c2eeea82db3f85b509c
Jerome <jerome@localhost.localdomain>
2022-06-15 01:39:47 -0700

    linux

--- 108_convert_sorted_array_to_BST.py
+++ 108_convert_sorted_array_to_BST.py
@@ -5,9 +5,27 @@ Created on Jun 14 04:37:19 2022
 @author: Jerome Yutai Shen
 
 """
+from typing import Optional, List
+from common_classes import BinaryTreeNode
 
 
+def sortedArrayToBST(nums: List[int]) -> BinaryTreeNode:
+    return helper(nums, 0, len(nums) - 1)
 
+
+def helper(nums: List[int], p_left: int, p_right: int) -> Optional[BinaryTreeNode]:
+    if p_left > p_right:
+        return None
+
+    # always choose left middle node as a root
+    p = (p_left + p_right) // 2
+
+    # preorder traversal: node -> left -> right
+    root = BinaryTreeNode(nums[p])
+    root.left = helper(nums, p_left, p - 1)
+    root.right = helper(nums, p + 1, p_right)
+    return root
 
 
 if __name__ == "__main__":
+    pass
---
+++ 278_first_bad_version.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jun 15 01:32:30 2022
+
+@author: Jerome Yutai Shen
+
+"""
+
+
+
+
+
+if __name__ == "__main__":
+    pass
