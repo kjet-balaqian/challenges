9dc9d162ac55e7ee3e3765e1aa94d4426455f70e
Jerome <jerome@localhost.localdomain>
2022-06-15 01:40:46 -0700

    linux

--- 278_first_bad_version.py
+++ 278_first_bad_version.py
@@ -7,7 +7,19 @@ Created on Jun 15 01:32:30 2022
 """
 
 
-
+def firstBadVersion(n: int) -> int:
+    start = 1
+    end = n
+    while start + 1 < end:
+        mid = (start + end) // 2
+        if isBadVersion(mid):
+            end = mid
+        else:
+            start = mid
+    if isBadVersion(start):
+        return start
+    elif isBadVersion(end):
+        return end
 
 
 if __name__ == "__main__":
